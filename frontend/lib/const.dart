92fcf555d91cc2bc0b09c43bc3ba7bf48638e574
Siddharth Bisht <siddharthbisht99@gmail.com>
2021-12-17 17:10:50 +0530

    initial commit

---
+++ frontend/lib/const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const darkModeMainBlack = Color(0xff131313);
+const darkModeSupportBlackishGrey = Color(0xff272727);
+const antiFlashWhite = Color(0xffDBDBDB);
+const white = Color(0xffffffff);
