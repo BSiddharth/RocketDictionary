7dbacef194244aa0de4aded79ce64526cbc294a2
Siddharth Bisht <siddharthbisht99@gmail.com>
2021-12-18 20:50:10 +0530

    implemented light mode toggle

--- frontend/lib/const.dart
+++ frontend/lib/const.dart
@@ -1,7 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 // Colors
 const kDarkModeMainBlack = Color(0xff131313);
 const kDarkModeSupportBlackishGrey = Color(0xff272727);
 const kAntiFlashWhite = Color(0xffDBDBDB);
 const kWhite = Color(0xffffffff);
+const kDarkModeGreen = Color(0xff36EA8E);
+const kDeepSkyBlue = Color(0xff3EAFEE);
+const kCerulean = Color(0xff1C76A5);
+const kCornFlowerBlue = Color(0xff7588E6);
+const kPrussianBlue = Color(0xff2F3952);
+
+// Light mode theme
+ThemeData lightThemeData = ThemeData(
+    unselectedWidgetColor: kPrussianBlue,
+    scaffoldBackgroundColor: kAntiFlashWhite,
+    primarySwatch: Colors.blue,
+    // drawerTheme: DrawerThemeData(),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: kWhite,
+      elevation: 0,
+      selectedItemColor: kCerulean,
+    ),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: kAntiFlashWhite,
+      elevation: 0,
+      systemOverlayStyle: SystemUiOverlayStyle(
+        // Status bar color
+        statusBarColor: kAntiFlashWhite, // For both Android + iOS
+
+        // Status bar brightness (optional)
+        statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
+        statusBarBrightness: Brightness.light, // For iOS (dark icons)
+      ),
+    ),
+    iconTheme: const IconThemeData(size: 28, color: kPrussianBlue,),
+    switchTheme: SwitchThemeData(
+      trackColor: MaterialStateProperty.all<Color>(Colors.grey),
+    ),
+    
+    );
+
+// Dark Mode theme
+ThemeData darkThemeData = ThemeData(
+    scaffoldBackgroundColor: kDarkModeMainBlack,
+    primarySwatch: Colors.blue,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: kDarkModeMainBlack,
+      elevation: 0,
+      systemOverlayStyle: SystemUiOverlayStyle(
+        // Status bar color
+        statusBarColor: kDarkModeMainBlack, // For both Android + iOS
+        statusBarIconBrightness: Brightness.light, // For Android
+        statusBarBrightness: Brightness.dark, // For iOS
+      ),
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: kDarkModeSupportBlackishGrey,
+      elevation: 0,
+      selectedItemColor: kDeepSkyBlue,
+      unselectedItemColor: kAntiFlashWhite,
+    ),
+    iconTheme: const IconThemeData(size: 28, color: kAntiFlashWhite),
+    switchTheme: SwitchThemeData(
+      trackColor: MaterialStateProperty.all<Color>(kAntiFlashWhite),
+    ));
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,58 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:rocketdictionary/const.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: kDarkModeMainBlack,
-  ));
+  // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+  //   statusBarColor: kDarkModeMainBlack,
+  // ));
+  
+  runApp(const ProviderScope(child: MyApp()));
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+final lightModeActivatedStateProvider = StateProvider<bool>((ref) => true);
 
-  // This widget is the root of your application.
+// final lightModeActivatedProvider = Provider<bool>((ref) => false);
+
+class MyApp extends ConsumerWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Rocket Dictionary',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: kDarkModeMainBlack,
-        primarySwatch: Colors.blue,
-        appBarTheme: const AppBarTheme(backgroundColor: kDarkModeMainBlack,elevation: 0,),
-        iconTheme: const IconThemeData(size: 28,color: kAntiFlashWhite)
-      ),
-      home: const HomePage(),
+      theme: ref.watch(lightModeActivatedStateProvider)
+          ? lightThemeData
+          : darkThemeData,
+      home: const MainPage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _MainPageState extends State<MainPage> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
-
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(
+      fontSize: 30, fontWeight: FontWeight.bold, color: kAntiFlashWhite);
   static const List<Widget> _widgetOptions = <Widget>[
     Text(
-      'Index 0: Home',
+      'Index 0: Rocket',
       style: optionStyle,
     ),
     Text(
-      'Index 1: Business',
+      'Index 1: Glossary',
       style: optionStyle,
     ),
     Text(
-      'Index 2: School',
+      'Index 2: Bookmarks',
       style: optionStyle,
     ),
   ];
@@ -65,36 +69,96 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final dimension = MediaQuery.of(context).size;
+
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
         centerTitle: true,
-        title: Image.asset("assets/images/RocketDictionaryLogo-removebg.png",height: 45,),
-        leading: IconButton(
-          onPressed: () {
-            _scaffoldKey.currentState?.openDrawer();
+        title: Consumer(
+          builder: (context, ref, child) {
+            return Image.asset(
+          "assets/images/RocketDictionaryLogo-removebg.png",
+          height: 45,
+          color: ref.watch(lightModeActivatedStateProvider)? kPrussianBlue:kAntiFlashWhite,
+        ) ;
           },
-          icon:const Icon(Icons.menu_rounded,size: 28,)
         ),
-        actions: const [Padding(
-          padding: EdgeInsets.symmetric(horizontal: 10),
-          child: Icon(Icons.filter_list_rounded),
-        ),Padding(
-          padding: EdgeInsets.symmetric(horizontal: 10),
-          child: Icon(Icons.search),
-        ),],
-
-        actionsIconTheme: const IconThemeData(size: 28,color: kAntiFlashWhite),
-        
+        leading: IconButton(
+          color: Theme.of(context).iconTheme.color,
+            onPressed: () {
+              _scaffoldKey.currentState?.openDrawer();
+            },
+            icon: const Icon(
+              Icons.menu_rounded,
+              size: 28,
+            )),
+        // leading: const Icon(
+        //       Icons.menu_rounded,
+        //       size: 28,
+        //       // color: kPrussianBlue,
+        //     ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.filter_list_rounded),
+            iconSize: 28,
+            color: kDarkModeGreen,
+          ),
+          Consumer(builder: (context, ref, child) {
+            final color = ref.watch(lightModeActivatedStateProvider)
+                ? kCerulean
+                : kDeepSkyBlue;
+            return IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search),
+              iconSize: 28,
+              color: color,
+            );
+          }),
+        ],
       ),
-      drawer: const Drawer(),
-       body: Center(
+      drawer: Consumer(
+        builder: (context, ref, child) {
+          return  Drawer(
+        child: Column(
+          children: [
+            Container(
+              color: ref.watch(lightModeActivatedStateProvider)? Colors.grey: kDarkModeSupportBlackishGrey,
+              height: 0.3 * dimension.height,
+              // height: 0.3 * dimension.height,
+            ),
+            Expanded(
+                child: Container(
+              width: double.infinity,
+              color: ref.watch(lightModeActivatedStateProvider)? kAntiFlashWhite: kDarkModeMainBlack,
+              child: Column(
+                children: const [
+                  DrawerSwitch(),
+                  Spacer(),
+                  FollowLinksAndCopyRight(),
+                ],
+              ),
+            ))
+          ],
+        ),
+      );
+        },
+      ),
+      
+     
+      body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            // icon: Image.asset(
+            //   'assets/icons/rocket.png',
+            //   color: _selectedIndex == 0 ? kDeepSkyBlue : kAntiFlashWhite,
+            //   height: 25,
+            // ),
+            icon: FaIcon(FontAwesomeIcons.rocket),
             label: 'Home',
           ),
           BottomNavigationBarItem(
@@ -107,11 +171,103 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: kAntiFlashWhite,
+        // selectedItemColor: kDeepSkyBlue,
+        // selectedItemColor: kCerulean,
+        // unselectedItemColor: kAntiFlashWhite,
         onTap: _onItemTapped,
-        backgroundColor: kDarkModeSupportBlackishGrey,
+        // backgroundColor: kDarkModeSupportBlackishGrey,
       ),
-   
+    );
+  }
+}
+
+class DrawerSwitch extends ConsumerWidget {
+  const DrawerSwitch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bool switchValue = !ref.watch(lightModeActivatedStateProvider);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Icon(
+            Icons.wb_sunny_outlined,
+            color: Colors.orange,
+            size: 34,
+          ),
+          const SizedBox(
+            width: 7,
+          ),
+          Transform.scale(
+            scale: 1.1,
+            child: Switch(
+              value: switchValue,
+              onChanged: (val) {
+                ref.read(lightModeActivatedStateProvider.state).state = !val;
+              },
+              activeColor: kCornFlowerBlue,
+              inactiveThumbColor: Colors.orange,
+            ),
+          ),
+          const SizedBox(
+            width: 7,
+          ),
+          const Icon(
+            Icons.nightlight_outlined,
+            color: kCornFlowerBlue,
+            size: 34,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class FollowLinksAndCopyRight extends ConsumerWidget {
+  const FollowLinksAndCopyRight({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context,WidgetRef ref) {
+    final Color textColor = ref.watch(lightModeActivatedStateProvider)? Colors.grey: kAntiFlashWhite;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      child: Column(
+        children: [
+           Text(
+            'Follow us on',
+            style: TextStyle(
+              color: textColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              FaIcon(FontAwesomeIcons.facebookF),
+              FaIcon(FontAwesomeIcons.twitter),
+              FaIcon(FontAwesomeIcons.instagram),
+              FaIcon(FontAwesomeIcons.linkedinIn),
+              FaIcon(FontAwesomeIcons.telegram),
+            ],
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+           Text(
+            'Ⓒ 2021 RocketDictionary® v0.0.10',
+            style: TextStyle(
+              color: textColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
