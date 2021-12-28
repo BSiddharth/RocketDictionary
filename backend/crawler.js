d5664fedc92b6319fc0faa3087aa3effb1ab9654
Siddharth Bisht <siddharthbisht99@gmail.com>
2021-12-28 12:59:42 +0530

    crawler works no bugs as of now

--- backend/crawler.js
+++ backend/crawler.js
@@ -25,60 +25,69 @@ const getRocketTitle = async () => {
         }
       }
     );
+    console.log(`length of rocketTitleSet is ${rocketTitleSet.size}`);
     return rocketTitleSet;
   } catch (error) {
+    console.log("Rocket title set retreivement (is that even a word) failed");
     console.log(error);
   }
 };
 const addRocketData = async () => {
+  var count = 0;
   try {
-    console.log("Adding data to the database");
     await connectToDB(process.env.connectionString);
+    console.log("Connected to the database");
     rocketTitleSet = await getRocketTitle();
-    await Promise.all(
-      [...rocketTitleSet].map(async (rocketTitle) => {
-        try {
-          console.log(`Adding ${rocketTitle} to the database`);
-          const page = await wiki().page(rocketTitle);
-          const summary = await page.summary();
-          const content = await page.content();
-          const mainImage = await page.mainImage();
-          const info = await page.info();
-          const title = page.title;
-          const rocket = new RocketModel({
-            name: title,
-            summary: summary,
-            content: content,
-            manufacturer:
-              typeof info["manufacturer"] === "object"
-                ? "Many"
-                : info["manufacturer"],
-            mainImage: mainImage,
-            countryOfOrigin:
-              typeof info["countryOrigin"] === "object"
-                ? "Many"
-                : info["countryOrigin"],
-            costPerLaunch:
-              typeof info["cpl"] === "object" ? "Varies" : info["cpl"],
-            type:
-              typeof info["function"] === "object"
-                ? "Launch Vehicle"
-                : info["function"],
-            status:
-              typeof info["status"] === "object" ? "Varies" : info["status"],
-          });
-          await rocket.save();
-          console.log(`${rocketTitle} was added to the database`);
-        } catch (error) {
-          console.log(error);
-        }
-      })
-    );
+    console.log("Rocket title set retreived");
+    console.log("Adding data to the database");
+    for (const rocketTitle of rocketTitleSet) {
+      try {
+        console.log(`Adding ${rocketTitle} to the database`);
+        const page = await wiki().page(rocketTitle);
+        const summary = await page.summary();
+        const content = await page.content();
+        const mainImage = await page.mainImage();
+        const info = await page.info();
+        const title = page.title;
+        const rocket = new RocketModel({
+          name: title,
+          summary: summary,
+          content: content,
+          manufacturer:
+            typeof info["manufacturer"] === "object"
+              ? "Many"
+              : info["manufacturer"],
+          mainImage: mainImage,
+          countryOfOrigin:
+            typeof info["countryOrigin"] === "object"
+              ? "Many"
+              : info["countryOrigin"],
+          costPerLaunch:
+            typeof info["cpl"] === "object" ? "Varies" : info["cpl"],
+          type:
+            typeof info["function"] === "object"
+              ? "Launch Vehicle"
+              : info["function"],
+          status:
+            typeof info["status"] === "object" ? "Varies" : info["status"],
+        });
+        await rocket.save();
+        console.log(`${rocketTitle} was added to the database`);
+        count++;
+      } catch (error) {
+        console.log(`${rocketTitle} was not added to the database!!!`);
+        console.log(error);
+      }
+    }
   } catch (error) {
+    console.log(
+      "Either connection to the DB failed or rocket title set retreivement (is that even a word) failed"
+    );
     console.log(error);
   } finally {
+    console.log(`${count} rockets added to the DB`);
     mongoose.connection.close();
-    console.log("Data added into the database");
+    console.log("DB connection closed");
   }
 };
 
